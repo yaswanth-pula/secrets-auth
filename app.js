afb4d3039de071549f728afeb700dd9df287c692
yaswanth-pula <35864576+Yaswanth899@users.noreply.github.com>
2020-10-22 18:11:44 +0530

    Added Level 1 Security
    -> Added email and password for Register and login a user to access secrets
    page.
    -> Used monogodb for storing user email and password.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,22 +9,74 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({ extended:true }));
 
-// home page
+///////////  DB Setup ///////////////
+
+// DB Name
+const dbName = "userDB";
+
+// DB URL
+const dbConnectionUrl = "mongodb://localhost:27017/"+dbName; 
+
+// Connect to DB
+mongoose.connect(dbConnectionUrl, {useNewUrlParser:true, useUnifiedTopology:true});
+
+// User Schema
+const userSchema = {
+    email:{type:String,required:true},
+    password:{type:String,required:true}
+} 
+
+// User Model
+const User = mongoose.model("User",userSchema);
+ 
+// home page route
 app.get("/",(req,res)=>{
     res.render("home")
 });
 
-// login page
+// login page route
 app.get("/login",(req,res)=>{
     res.render("login")
 });
 
-// register page
+// register page route
 app.get("/register",(req,res)=>{
     res.render("register");
 });
 
+// register post route - New User Registration
+app.post("/register",(req,res)=>{
+    const userEmail = req.body.username;
+    const userPassword = req.body.password;
+    // new user
+    const newUser = new User({
+        email:userEmail,
+        password:userPassword
+    });
+    // save new user into DB
+    newUser.save((err)=>{
+        if(err) console.log(err);
+        else res.render("/secrets");
+    });
+});
 
+// login post route - User Login
+app.post("/login",(req,res)=>{
+    const userEmail = req.body.username;
+    const userPassword = req.body.password;
+    // check for the user in DB
+    User.findOne({email:userEmail},(err,resultUser)=>{
+        if(err) console.log(err)
+        else{
+            if(resultUser){ 
+                if(resultUser.password === userPassword)
+                    res.render("secrets");
+                else 
+                    res.redirect("/login"); 
+            }
+        }
+    })
+})
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server is Up and Running.");
